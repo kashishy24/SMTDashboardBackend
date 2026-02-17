c662fa44497a3c5a41fa83ecf7a0ff2bb259bdb9
kashishy24 <kashishy2403.com>
2026-02-17 11:28:07 +0530

    Make the Assembly wise oee api for line

--- src/Controllers/HomePage/Home.js
+++ src/Controllers/HomePage/Home.js
@@ -36,7 +36,38 @@ router.get("/plant-smt-oee", async (req, res) => {
   }
 });
 
+router.get("/plant-assembly-oee", async (req, res) => {
+  try {
+    const {
+      filterType,
+      shift = null,
+      startDate = null,
+      endDate = null,
+    } = req.query;
 
+    if (!filterType) {
+      return res.status(400).json({
+        message: "filterType is required",
+      });
+    }
+
+    const request = new sql.Request();
+
+    request.input("FilterType", sql.VarChar(20), filterType);
+    request.input("Shift", sql.Char(1), shift);
+    request.input("StartDate", sql.Date, startDate);
+    request.input("EndDate", sql.Date, endDate);
+
+    const result = await request.execute(
+      "SP_Dashboard_PlantLevel_Assembly_OEE"
+    );
+
+    res.json(result.recordset);
+  } catch (err) {
+    console.error("Error in GET /plant-assembly-oee", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 
 
